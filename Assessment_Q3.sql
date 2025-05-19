2ad2054f9d4e05e05168e6457a449cb1f809ef09
The-aniel24 <105610105+oedaniel24@users.noreply.github.com>
2025-05-19 14:38:18 +0100

    Add files via upload

---
+++ Assessment_Q3.sql
@@ -0,0 +1,37 @@
+-- Step 1: Get the latest transaction per owner
+WITH latest_transactions AS (
+    SELECT
+        owner_id,
+        MAX(transaction_date) AS last_transaction_date
+    FROM
+        savings_savingsaccount
+    GROUP BY
+        owner_id
+)
+
+-- Step 2: Join with the original tables to get full details
+SELECT 
+    P.id AS plan_id,
+    S.owner_id,
+    CASE
+        WHEN P.is_regular_savings = 1 THEN 'Savings'
+        WHEN P.is_a_fund = 1 THEN 'Investment'
+    END AS type,
+    S.transaction_date AS last_transaction_date,
+
+-- Calculating inactivity dates as the number of days between the last transaction date and the current date
+    DATEDIFF(CURDATE(), S.transaction_date) AS inactivity_days 
+FROM
+    savings_savingsaccount S
+JOIN
+    latest_transactions LT ON S.owner_id = LT.owner_id AND S.transaction_date = LT.last_transaction_date
+LEFT JOIN
+    plans_plan P ON P.id = S.plan_id
+WHERE
+
+-- specifying only transactions that are either savings plan or investment plan
+    (P.is_a_fund = 1 OR P.is_regular_savings = 1)
+    
+--  Specifying only active accounts (savings or investments) with no transactions in the last 1 year (365 days) .
+    AND
+    DATEDIFF(CURDATE(), S.transaction_date) >  365;
